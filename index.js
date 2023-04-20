f75f1ee0965a06c10b07ea23e52113d41a26c393
Rafael Sanzio <rafaelsanzio27@gmail.com>
2023-04-20 19:46:41 -0300

    feat: index to start server
    index to start server

---
+++ index.js
@@ -0,0 +1,46 @@
+import express from "express";
+import winston from "winston";
+import path from "path";
+import { promises } from "fs";
+
+import products from "./routes/products.js";
+
+const fs = promises;
+const app = express();
+
+const { combine, timestamp, label, printf } = winston.format;
+const logFormat = printf(({ level, message, label, timestamp }) => {
+  return `${timestamp} [${label}] ${level}: ${message}`;
+});
+
+const jsonFile = path.join(process.cwd(), "./data/products.json");
+
+global.productsFileJSON = jsonFile;
+global.logger = winston.createLogger({
+  level: "silly",
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({ filename: "products-api.log" }),
+  ],
+  format: combine(label({ label: "products-api" }), timestamp(), logFormat),
+});
+
+app.use(express.json());
+app.get("/", function (req, res) {
+  res.redirect("/products");
+});
+app.use("/products", products);
+
+app.listen(3000, async () => {
+  try {
+    await fs.readFile(productsFileJSON, "utf-8");
+  } catch (err) {
+    const initalJson = {
+      nextID: 1,
+      products: [],
+    };
+    fs.writeFile(productsFileJSON, JSON.stringify(initalJson)).catch((err) => {
+      logger.error(err);
+    });
+  }
+});
